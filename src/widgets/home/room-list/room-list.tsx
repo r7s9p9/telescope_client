0cfbcb1983b0c59371261d198bf130795d26aafc
r7s9p9 <118193598+r7s9p9@users.noreply.github.com>
2024-04-04 16:26:42 +0300

    refactored RoomList component

--- src/widgets/home/room-list/room-list.tsx
+++ src/widgets/home/room-list/room-list.tsx
@@ -1,31 +1,41 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { RoomInListType, RoomListType } from "../../../shared/api/api.schema";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, useCallback, useEffect, useMemo, useState } from "react";
 import { formatDate } from "../../../shared/lib/date";
 import { useNotify } from "../../notification/notification";
 import { useQueryRoomList } from "../../../shared/api/api";
 import { getRandomInt } from "../../../shared/lib/random";
 import { checkRoomId } from "../../../shared/lib/uuid";
 import { routes } from "../../../constants";
-import throttle from "../../../shared/lib/throttle";
 import { debounce } from "../../../shared/lib/debounce";
-
-type ListType = HTMLUListElement;
-type ListRef = React.RefObject<ListType>;
+import React from "react";
 
 const itemHeight = 64 as const; // for skeleton & item
 
-const overscreenItemCountForInitialLoading = 6 as const;
-const overscreenItemCountToTriggerFurtherLoading = 4 as const;
-const overscreenItemCountForFurtherLoading = 6 as const;
-const debounceScrollDelay = 200 as const;
+const itemCountForInitialLoading = 20 as const;
+const overscreenItemCountToTriggerFurtherLoading = 5 as const;
+const overscreenItemCountForFurtherLoading = 10 as const;
+const debounceScrollDelay = 100 as const;
+
+const SkeletonList = React.memo(({ count }: { count: number }) => {
+  if (count > 0) {
+    const skeletonItems = Array(count)
+      .fill(1)
+      .map((_, i) => (
+        <li key={i}>
+          <ItemSkeleton />
+        </li>
+      ));
+    return skeletonItems;
+  }
+  return null;
+});
 
 export function RoomList() {
   const notify = useNotify();
   const navigate = useNavigate();
   const query = useQueryRoomList();
   const { roomId } = useParams();
-  const listRef = useRef<ListType>(null);
   const [listData, setListData] = useState<RoomListType>();
 
   const isInitialLoading = !listData?.roomDataArr;
@@ -34,91 +44,119 @@ export function RoomList() {
     listData && listData.allCount === listData.roomDataArr?.length
   );
 
-  const queryAction = async (min: number, max: number) => {
-    const { success, data } = await query.run({
-      min: min.toString(),
-      max: max.toString(),
-    });
-    if (!success) notify.show.error("ROOM LIST NO SUCCESS");
-    if (success) {
-      if (isInitialLoading) setListData(data);
-      if (
-        listData?.roomDataArr &&
-        data.roomDataArr &&
-        max > data.roomDataArr.length
-      ) {
-        setListData({
-          ...listData,
-          allCount: data.allCount,
-          dev: data.dev,
-          roomDataArr: listData.roomDataArr.concat(data.roomDataArr),
-        });
+  const queryAction = useCallback(
+    async (min: number, max: number) => {
+      const { success, data } = await query.run({
+        min: min.toString(),
+        max: max.toString(),
+      });
+      if (!success) notify.show.error("ROOM LIST NO SUCCESS");
+      if (success) {
+        if (isInitialLoading) setListData(data);
+        if (
+          listData?.roomDataArr &&
+          data.roomDataArr &&
+          max > data.roomDataArr.length
+        ) {
+          setListData({
+            ...listData,
+            allCount: data.allCount,
+            dev: data.dev,
+            roomDataArr: listData.roomDataArr.concat(data.roomDataArr),
+          });
+        }
       }
-    }
-  };
+    },
+    [query],
+  );
 
-  function debouncedQueryAction(min: number, max: number, delay: number) {
-    debounce(() => {
-      queryAction(min, max);
-    }, delay);
-  }
+  //
+  const revalidateInterval = 10000 as const;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(listData?.roomDataArr?.length);
+      if (listData?.roomDataArr) {
+        const max = listData.roomDataArr.length;
+        queryAction(0, max);
+      }
+    }, revalidateInterval);
+    return () => clearInterval(interval);
+  }, [listData]);
 
   // Initial load data
   useEffect(() => {
     // wrong roomId protection
     if (roomId && !checkRoomId(roomId)) navigate(routes.home.path);
-    if (!listData && listRef.current) {
-      const onscreenItemCount = Math.ceil(
-        listRef.current.offsetHeight / itemHeight,
-      );
-      const stopItem = onscreenItemCount + overscreenItemCountForInitialLoading;
-      queryAction(0, stopItem);
+    if (!listData) {
+      queryAction(0, itemCountForInitialLoading);
     }
   }, []);
 
-  const debouncedHandleScroll = debounce(() => {
-    if (
-      !isInitialLoading &&
-      !isAllLoaded &&
-      !isZeroItemCount &&
-      listRef.current &&
-      listData?.roomDataArr
-    ) {
-      const lastVisibleIndex = Math.ceil(
-        (listRef.current.scrollTop + listRef.current.offsetHeight) / itemHeight,
-      );
-      const lastLoadedIndex = listData.roomDataArr.length;
-      const isNeedToLoad = !!(
-        lastLoadedIndex - lastVisibleIndex <
-        overscreenItemCountToTriggerFurtherLoading
-      );
+  const handleScroll = useCallback(
+    (e: React.UIEvent<HTMLElement>) => {
+      if (
+        !isInitialLoading &&
+        !isAllLoaded &&
+        !isZeroItemCount &&
+        listData?.roomDataArr
+      ) {
+        const target = e.nativeEvent.target as HTMLElement;
+        const lastVisibleIndex = Math.ceil(
+          (target.scrollTop + target.offsetHeight) / itemHeight,
+        );
+        const lastLoadedIndex = listData.roomDataArr.length;
+        const isNeedToLoad = !!(
+          lastLoadedIndex - lastVisibleIndex <
+          overscreenItemCountToTriggerFurtherLoading
+        );
 
-      if (isNeedToLoad) {
-        const startItem = lastLoadedIndex;
-        if (
-          lastLoadedIndex + overscreenItemCountForFurtherLoading >
-          listData.allCount
-        ) {
-          queryAction(lastLoadedIndex, listData.allCount);
-        }
-        if (lastVisibleIndex > lastLoadedIndex) {
-          const stopItem =
-            lastVisibleIndex + overscreenItemCountForFurtherLoading;
-          stopItem > listData.allCount
-            ? queryAction(startItem, listData.allCount)
-            : queryAction(startItem, stopItem);
-        } else {
-          const stopItem = startItem + overscreenItemCountForFurtherLoading;
-          queryAction(startItem, stopItem);
+        if (isNeedToLoad) {
+          const startItem = lastLoadedIndex;
+          if (
+            lastLoadedIndex + overscreenItemCountForFurtherLoading >
+            listData.allCount
+          ) {
+            queryAction(lastLoadedIndex, listData.allCount);
+          }
+          if (lastVisibleIndex > lastLoadedIndex) {
+            const stopItem =
+              lastVisibleIndex + overscreenItemCountForFurtherLoading;
+            stopItem > listData.allCount
+              ? queryAction(startItem, listData.allCount)
+              : queryAction(startItem, stopItem);
+          } else {
+            const stopItem = startItem + overscreenItemCountForFurtherLoading;
+            queryAction(startItem, stopItem);
+          }
         }
       }
-    }
-  }, debounceScrollDelay);
+    },
+    [
+      isAllLoaded,
+      isInitialLoading,
+      isZeroItemCount,
+      listData?.allCount,
+      listData?.roomDataArr,
+      queryAction,
+    ],
+  );
 
-  if (isZeroItemCount) return <ListEmpty listRef={listRef} />;
+  const debouncedHandleScroll = useMemo(
+    () => debounce(handleScroll, debounceScrollDelay),
+    [handleScroll],
+  );
 
-  if (!listData?.roomDataArr || isInitialLoading)
-    return <ListSkeleton listRef={listRef} />;
+  if (isZeroItemCount) return <ListEmpty />;
+
+  const count = getRandomInt(4, 12);
+  if (!listData?.roomDataArr || isInitialLoading) {
+    return (
+      <ListWrapper>
+        <SkeletonList count={count} />
+      </ListWrapper>
+    );
+  }
 
   const items = listData.roomDataArr.map((itemData) => (
     <li key={itemData.roomId}>
@@ -126,57 +164,40 @@ export function RoomList() {
     </li>
   ));
 
-  const skeletonAdder = () => {
-    if (listData.roomDataArr) {
-      const count = listData.allCount - listData.roomDataArr.length;
-      const skeletonItems = Array(count)
-        .fill(1)
-        .map((_, i) => (
-          <li key={i}>
-            <ItemSkeleton />
-          </li>
-        ));
-      return items.concat(skeletonItems);
-    }
-  };
-
   return (
-    <ListWrapper listRef={listRef} handleScroll={debouncedHandleScroll}>
-      {isAllLoaded ? items : skeletonAdder()}
+    <ListWrapper onScroll={debouncedHandleScroll}>
+      {items}
+      {!isAllLoaded && (
+        <SkeletonList count={listData.allCount - listData.roomDataArr.length} />
+      )}
     </ListWrapper>
   );
 }
 
 function ListWrapper({
   children,
-  listRef,
-  handleScroll,
+  onScroll,
 }: {
   children: ReactNode;
-  listRef: ListRef;
-  handleScroll?: ReturnType<typeof Function>;
+  onScroll?: (e: React.UIEvent<HTMLElement>) => void;
 }) {
-  function onScroll() {
-    if (handleScroll) handleScroll();
-  }
-
   return (
     <ul
-      onScroll={() => onScroll()}
-      onClick={() => onScroll()}
+      onScroll={onScroll}
       className="overflow-y-scroll overscroll-none scroll-smooth w-full h-full flex flex-col bg-slate-50"
-      ref={listRef}
     >
       {children}
     </ul>
   );
 }
 
-function ListEmpty({ listRef }: { listRef: ListRef }) {
+const itemHeightStyle = { height: itemHeight + "px" };
+
+function ListEmpty() {
   return (
-    <ListWrapper listRef={listRef}>
+    <ListWrapper>
       <p
-        style={{ height: itemHeight + "px" }}
+        style={itemHeightStyle}
         className="w-full text-center p-4 font-thin text-xl bg-slate-100"
       >
         You have no rooms
@@ -185,30 +206,15 @@ function ListEmpty({ listRef }: { listRef: ListRef }) {
   );
 }
 
-function ListSkeleton({ listRef }: { listRef: ListRef }) {
-  const count = getRandomInt(4, 12);
-  return (
-    <ListWrapper listRef={listRef}>
-      {Array(count)
-        .fill(1)
-        .map((_, i) => (
-          <li key={i}>
-            <ItemSkeleton />
-          </li>
-        ))}
-    </ListWrapper>
-  );
-}
-
 function ItemSkeleton() {
   return (
     <div
-      style={{ height: itemHeight + "px" }}
+      style={itemHeightStyle}
       className="w-full flex flex-col px-4 py-2 justify-between bg-slate-50 border-b-2 border-slate-200"
     >
       <div className="flex justify-between animate-pulse">
         <div className="rounded-md h-4 w-2/5 bg-slate-200"></div>
-        <div className="w-16 rounded-md h-4 w-1/5 bg-slate-200"></div>
+        <div className="rounded-md h-4 w-1/5 bg-slate-200"></div>
       </div>
       <div className="h-4 w-3/5 rounded-md bg-slate-200 animate-pulse"></div>
     </div>
@@ -227,7 +233,7 @@ function Item({ isOpened, data }: { isOpened: boolean; data: RoomInListType }) {
   return (
     <Link to={"/room/" + data.roomId}>
       <button
-        style={{ height: itemHeight + "px" }}
+        style={itemHeightStyle}
         className={`${isOpened ? "bg-slate-200 hover:bg-slate-200 cursor-default" : "bg-slate-50 hover:bg-slate-200"} w-full flex flex-col py-1 px-4 justify-between items-center border-b-2 border-slate-200 duration-300 ease-out`}
       >
         <div className="w-full flex flex-row justify-between items-center gap-2">
